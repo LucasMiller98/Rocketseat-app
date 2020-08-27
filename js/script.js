5a526dfef461b5e346498a581a8b9204cae76a09
LucasMiller98 <lucas.miller1998@outlook.com>
2020-08-27 14:20:13 -0300

    Adicionado o inicio de uma aplicação

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let listElement = document.querySelector('#app ul')
+let inputElement = document.querySelector('#app input')
+let buttonElement = document.querySelector('#app button')
+
+// Para renderizar a lista adicionando os novos to do
+
+// como é um simples texto
+let to_do = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocketseat'
+]
